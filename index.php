b6725154bf6a32b441741d3aaaa57424ff4e6e3b
Vryllon <noahvader@yahoo.com>
2024-12-01 09:43:45 -0500

    moved index to the top of file heirchy for starting website

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Caregiver Website</title>
+</head>
+
+<body>
+    <div>
+        <h1>Welcome to Caregiver Website by Onur and Noah</h1>
+        <a href="frontend/register.php" class="button">Register</a>
+        <a href="frontend/login.php" class="button">Login</a>
+    </div>
+</body>
